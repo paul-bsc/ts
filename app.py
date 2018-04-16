2ba79435a35ebd41b1bc3f9d7bd2b53319e9fb0e
Paul <pdsantos@bscglobal.com>
2018-04-16 21:19:37 +0200

    Skeleton growing
    Added some docstrings, wrappers for the API calls and birthday helper function

--- app.py
+++ app.py
@@ -24,6 +24,11 @@ def welcome():
 # Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """Log a user in to the microsite
+
+    Returns:
+        html-page: Returns the login page
+    """
     error = None
     if request.method == 'POST':
         username = request.form['username']
@@ -41,12 +46,28 @@ def login():
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
+    """Log a user out of the microsite
+
+    Returns:
+        html-page: Returns the login page
+    """
     error = None
     session.clear()
     return redirect(url_for('login'))
 
 
 def get_token(username, password):
+    """Get the sign-in token
+
+    Args:
+        username (string): Provided username.
+        password (string): Provided password.
+
+    Returns:
+        bool: True if successful, otherwise false
+        response (string): Returns the token or the returned error message
+    """
+
     url = "http://staging.tangent.tngnt.co/api-token-auth/"
 
     payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; " \
@@ -67,6 +88,59 @@ def get_token(username, password):
         return False, response['non_field_errors']
     else:
         return False, None
+
+
+def get_user(token):
+    url = "http://staging.tangent.tngnt.co/api/user/me/"
+
+    headers = {
+        'authorization': "Token {}".format(token),
+        'cache-control': "no-cache"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    return response.json()[0]
+
+
+def get_user_full(token):
+    url = "http://staging.tangent.tngnt.co/api/employee/me/"
+
+    headers = {
+        'authorization': "Token {}".format(token),
+        'cache-control': "no-cache"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    return response.json()[0]
+
+
+def get_employee(token, race=None, position=None, start_date=None, user=None, gender=None, birth_date=None, email=None):
+    url = "http://staging.tangent.tngnt.co/api/employee/"
+
+    headers = {
+        'authorization': "Token {}".format(token),
+        'race': race,
+        'position': position,
+        'start_date': start_date,
+        'user': user,
+        'gender': gender,
+        'birth_date': birth_date,
+        'email': email,
+        'cache-control': "no-cache"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    return response.json()
+
+
+def birthday_coming_up(employees, days):
+    birthdays_coming_up = []
+    for employee in employees:
+        if employee['days_to_birthday'] - days <= 0:
+            birthdays_coming_up.append(employee['user']['id'])
 
 
 # start the server with the 'run()' method
